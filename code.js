9ef59d91b54b379052820d7371704020a7d4cff1
Christian Francis <lt.francis514@gmail.com>
2024-03-24 21:39:44 +0200

    initial commit of Odin dashboard project

---
+++ code.js
@@ -0,0 +1,18 @@
+function openrecipe(){
+    window.open('https://chrisfranco961.github.io/odin-recipes/')
+}
+function opensignup(){
+    window.open('https://chrisfranco961.github.io/odin_signup_form/')
+}
+function opensketch(){
+    window.open('https://chrisfranco961.github.io/Etch_a_sketch/')
+}
+function openhome(){
+    window.open('https://chrisfranco961.github.io/odin_project-homepage/')
+}
+function openrock(){
+    window.open('https://chrisfranco961.github.io/rock_paper_scissors/')
+}
+function opencalc(){
+    window.open('https://chrisfranco961.github.io/Calculator/')
+}
